e77d451be3f1f9a8a182222ab6002d2d4fdb58f8
Chatham Abate <chathamabate@gmail.com>
2026-07-28 17:49:59 +0000

    directory rework

--- after/plugin/color.lua
+++
@@ -1,7 +0,0 @@
-function SetColor(color)
-    color = color or "gruvbox-material" -- have a default value
-    vim.cmd.colorscheme(color)
-    vim.o.background = "dark"
-end
-
-SetColor() -- run at startup
